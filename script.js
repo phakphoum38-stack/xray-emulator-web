b2c3766c77ab3e135e14b43895c1efb2be8f114c
phakphoum38-stack <phakphoum38@gmail.com>
2026-03-25 19:18:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function scan(){
 
     let img = document.getElementById("bag")
 
-    img.style.filter = "invert(1) brightness(2)"
+    img.style.transition = "all 1s"
+    img.style.filter = "invert(1) brightness(2) contrast(2)"
 
     setTimeout(()=>{
         img.style.filter = "grayscale(100%)"
